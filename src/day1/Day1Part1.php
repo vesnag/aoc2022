18da55770f6d71d60e19cff6c6471b6598a6c675
Vesna Groselj <vgroselj@gmail.com>
2022-12-01 18:37:47 +0100

    Day1 part1 solution

---
+++ src/day1/Day1Part1.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AOC2022\day1;
+
+final class Day1Part1
+{
+  /**
+   * @param array<int,mixed> $data
+   */
+    public static function getMaxSum(array $data): int
+    {
+        $maxSum = 0;
+        $currentSum = 0;
+
+        foreach ($data as $item) {
+          if (!is_numeric($item)) {
+            if ($currentSum > $maxSum) {
+              $maxSum = $currentSum;
+            }
+            $currentSum = 0;
+            continue;
+          }
+          $currentSum += (int) $item;
+        }
+
+        return $maxSum;
+    }
+}
---
+++ tests/Day1Part1Test.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use AOC2022\day1\Day1Part1;
+use PHPUnit\Framework\TestCase;
+
+final class Day1Part1Test extends TestCase {
+
+  public function testMaxSum(): void {
+    $this->assertEquals(
+      24000,
+      Day1Part1::getMaxSum($this->readInput('test-input.txt'))
+    );
+
+    $this->assertEquals(
+      70698,
+      Day1Part1::getMaxSum($this->readInput('input.txt'))
+    );
+  }
+
+  /**
+   * @return array<int,mixed>
+   */
+  private function readInput(string $filename): array
+  {
+    if ($fileContent = file_get_contents('input/day1/' . $filename)) {
+      return explode("\n", $fileContent);
+    }
+
+    return [];
+  }
+
+}
