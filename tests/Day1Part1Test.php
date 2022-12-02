f0e42ebde2d04cdc896f6dadcf2349b805a2efc1
Vesna Groselj <vgroselj@gmail.com>
2022-12-02 13:23:55 +0100

    CS fixes.

--- tests/Day1Part1Test.php
+++ tests/Day1Part1Test.php
@@ -5,19 +5,18 @@ declare(strict_types=1);
 use AOC2022\day1\Day1Part1;
 use PHPUnit\Framework\TestCase;
 
-final class Day1Part1Test extends TestCase {
+final class Day1Part1Test extends TestCase
+{
+    public function testMaxTotalCaloriesPart1(): void
+    {
+        $this->assertEquals(
+            24000,
+            Day1Part1::getMaxTotalCalories('test-input.txt')
+        );
 
-  public function testMaxTotalCaloriesPart1(): void
-  {
-    $this->assertEquals(
-      24000,
-      Day1Part1::getMaxTotalCalories('test-input.txt')
-    );
-
-    $this->assertEquals(
-      70698,
-      Day1Part1::getMaxTotalCalories('input.txt')
-    );
-  }
-
+        $this->assertEquals(
+            70698,
+            Day1Part1::getMaxTotalCalories('input.txt')
+        );
+    }
 }
--- tests/Day1Test.php
+++ tests/Day1Test.php
@@ -5,34 +5,33 @@ declare(strict_types=1);
 use AOC2022\day1\Day1;
 use PHPUnit\Framework\TestCase;
 
-final class Day1Test extends TestCase {
+final class Day1Test extends TestCase
+{
+    public function testMaxTotalCalories(): void
+    {
+        $this->assertEquals(
+            24000,
+            Day1::getMaxTotalCalories('test-input.txt', 1)
+        );
 
-  public function testMaxTotalCalories(): void
-  {
-    $this->assertEquals(
-     24000,
-     Day1::getMaxTotalCalories('test-input.txt', 1)
-    );
+        $this->assertEquals(
+            70698,
+            Day1::getMaxTotalCalories('input.txt', 1)
+        );
 
-    $this->assertEquals(
-      70698,
-      Day1::getMaxTotalCalories('input.txt', 1)
-    );
+        $this->assertEquals(
+            45000,
+            Day1::getMaxTotalCalories('test-input.txt', 3)
+        );
 
-    $this->assertEquals(
-    45000,
-      Day1::getMaxTotalCalories('test-input.txt', 3)
-    );
+        $this->assertEquals(
+            206643,
+            Day1::getMaxTotalCalories('input.txt', 3)
+        );
 
-    $this->assertEquals(
-      206643,
-      Day1::getMaxTotalCalories('input.txt', 3)
-    );
-
-    $this->assertEquals(
-      0,
-      Day1::getMaxTotalCalories('input.txt', 0)
-    );
-  }
-
+        $this->assertEquals(
+            0,
+            Day1::getMaxTotalCalories('input.txt', 0)
+        );
+    }
 }
