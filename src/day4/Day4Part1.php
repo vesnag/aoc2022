c77440a1091f98c7fc503fdb03b1d2ee3964bff9
Vesna Groselj <vgroselj@gmail.com>
2022-12-09 22:58:00 +0100

    Day 4 part1 solution.

---
+++ src/day4/Day4Part1.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AOC2022\day4;
+
+final class Day4Part1
+{
+    public static function countOverlappedAssignments(string $filename): int
+    {
+        $handle = fopen('input/day4/' . $filename, 'r');
+        if (!$handle) {
+            return 0;
+        }
+
+        $count = 0;
+        while (($line = fgets($handle)) !== false) {
+            $sections = explode(',', $line);
+            $firstSection = explode('-', $sections[0]);
+            $secondSection = explode('-', $sections[1]);
+
+            $len1 = (int) $firstSection[1] - (int) $firstSection[0];
+            $len2 = (int) $secondSection[1] - (int) $secondSection[0];
+
+            if ($len1 === $len2) {
+                if ($firstSection[0] == $secondSection[0]) {
+                    $count++;
+                }
+                continue;
+            }
+
+            if ($len1 < $len2) {
+                if (($firstSection[0] >= $secondSection[0]) && ($firstSection[1] <= $secondSection[1])) {
+                    $count++;
+                }
+                continue;
+            }
+
+            if (($firstSection[0] <= $secondSection[0]) && ($firstSection[1] >= $secondSection[1])) {
+                $count++;
+            }
+        }
+
+        return $count;
+    }
+}
---
+++ tests/Day4Part1Test.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use AOC2022\day4\Day4Part1;
+use PHPUnit\Framework\TestCase;
+
+final class Day4Part1Test extends TestCase
+{
+    public function testCountOverlappedAssignments(): void
+    {
+        $this->assertEquals(
+            2,
+            Day4Part1::countOverlappedAssignments('test-input.txt')
+        );
+
+        $this->assertEquals(
+          540,
+            Day4Part1::countOverlappedAssignments('input.txt')
+        );
+    }
+}
