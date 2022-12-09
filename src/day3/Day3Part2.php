6dd138a2638b9e10c1e755acc3f0f1184ee0119d
Vesna Groselj <vgroselj@gmail.com>
2022-12-09 21:10:15 +0100

    Solutions for Day3.

---
+++ src/day3/Day3Part2.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace AOC2022\day3;
+
+final class Day3Part2
+{
+    public static function getSumOfGroupBadges(string $filename, int $numberOfRucksack): int
+    {
+        $handle = fopen('input/day3/' . $filename, 'r');
+        if (!$handle) {
+            return 0;
+        }
+
+        $rucksackGroup = [];
+        $sum = 0;
+        $i = 0;
+
+        $minRucksackSize = 0;
+        while (($line = fgets($handle)) !== false) {
+            $line = trim($line);
+
+            if ($i == $numberOfRucksack) {
+                $sum += self::getSumOfGroupBadge($rucksackGroup);
+
+                $minRucksackSize = 0;
+                $rucksackGroup = [];
+                $i = 0;
+            }
+
+            if (0 === $i) {
+                $minRucksackSize = strlen($line);
+                $rucksackGroup[] = $line;
+                $i++;
+                continue;
+            }
+
+            $rucksackSize = strlen($line);
+            if ($rucksackSize < $minRucksackSize) {
+                array_unshift($rucksackGroup, $line);
+                $minRucksackSize = $rucksackSize;
+            } else {
+                $rucksackGroup[] = $line;
+            }
+            $i++;
+        }
+        $sum += self::getSumOfGroupBadge($rucksackGroup);
+
+        return $sum;
+    }
+
+    /**
+     * @param array<int, string> $rucksackGroup
+    */
+    private static function getSumOfGroupBadge(array $rucksackGroup): int
+    {
+        $i = 0;
+        $len = strlen($rucksackGroup[0]);
+        while ($i < $len) {
+            $currentCharacter = mb_substr($rucksackGroup[0], $i, 1);
+            if (str_contains($rucksackGroup[1], $currentCharacter) && str_contains($rucksackGroup[2], $currentCharacter)) {
+                return self::getCharValue($currentCharacter);
+            }
+            $i++;
+        }
+
+        return 0;
+    }
+
+
+    private static function getCharValue(string $char): int
+    {
+        $decimalValue = ord($char);
+        if ($decimalValue >= 65 &&  $decimalValue <= 90) {
+            return $decimalValue - 38;
+        }
+        if ($decimalValue >= 97 &&  $decimalValue <= 122) {
+            return $decimalValue - 96;
+        }
+
+        return 0;
+    }
+}
---
+++ tests/Day3Part2Test.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use AOC2022\day3\Day3Part2;
+use PHPUnit\Framework\TestCase;
+
+final class Day3Part2Test extends TestCase
+{
+    public function testSumOfGroupBadges(): void
+    {
+        $this->assertEquals(
+            70,
+            Day3Part2::getSumOfGroupBadges('test-input.txt', 3)
+        );
+
+        $this->assertEquals(
+            8349,
+            Day3Part2::getSumOfGroupBadges('input.txt', 3)
+        );
+    }
+}
